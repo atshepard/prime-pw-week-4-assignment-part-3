dff09135a3885a346f38d76e5fe6ae66127d0309
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-02-12 14:30:59 -0600

    testing new addItems function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -7,13 +7,13 @@ console.log('***** Cart Functions *****');
 let basket = []; //makes a basket
 const maxItems = 5;
 
-//
 // - Create a function called `addItem`. It should:
 //   - take an input parameter for a string `item`
 //   - add the new item to the global array `basket`.
 //   - return `true` indicating the item was added
+
 function isFull() {
-  console.log('in isFull')
+  //console.log('in isFull') //used to test that function is running
   if (basket.length < maxItems) {
     return false;
   } else if (basket.length >= maxItems) {
@@ -21,16 +21,28 @@ function isFull() {
   } // end conditional
 } // end isFull
 
-function addItem ( item ) {
-  console.log('in addItem') //will log if function is running
-  basket.push( item );
+// function addItem ( item ) {
+//   console.log('in addItem') //will log if function is running
+//   basket.push( item );
+// }// end addItem
 
-  return true;
+function addItem ( item ) {
+  //console.log('in addItem') //used to test function was running
+  if (isFull()) {
+    console.log('The basket is full if this is false:')
+    return false;
+  } else {
+    basket.push( item );
+    console.log('An item has been added if this is true:')
+    return true;
+  }
 }// end addItem
 
 addItem('banana'); //adds banana
+console.log('There should be a banana in the basket:', basket);//checking banana was added
 addItem('avocado'); //adds avocado
-console.log(addItem('bacon')); //adds bacon and returns true
+console.log('There should be an avocado in the basket:', basket);//checking avocado was added
+addItem('bacon'); //adds bacon
 console.log(basket); //should include 'banana' and 'avocado' and 'bacon' in that order. Success!
 //
 // - Create a function called `listItems`. It should:
@@ -42,24 +54,19 @@ console.log(basket); //should include 'banana' and 'avocado' and 'bacon' in that
 //   for (let i = 0; i < array.length; i++) {
 //     // console.log(array[i]);
 //   }
-//   return true;
+//   return true; //shows the loop has completed and the function works.
 // } //This code works, but I want to try something I saw on the old google machine:
 
-// function listItems (array) {
-//   console.log('in listItem');
-//   array.forEach((item) => {
-//     console.log(item)
-//   });
-//   return true;
-// } //end listItems.
-//This also works! Love that - the forEach method seems intuitive to me.
-
 function listItems (array) {
-  for (i = 0, i < array.length; i++) {
-    let itemInBasket = array[i];
-  }
-  return itemInBasket;
-}
+  console.log('in listItem');
+  let totalItems = 0;
+  array.forEach((item) => {
+    console.log(item)
+    totalItems++;
+  });
+  return totalItems; // shows total number of items in the basket.
+} //end listItems.
+//This also works! Love that - the forEach method seems intuitive to me.
 
 console.log(listItems(basket));
 console.log(basket); //checking the basket...
@@ -68,12 +75,11 @@ console.log(basket); //checking the basket...
 //   - reset the `basket` to an empty array
 function empty () {
   basket = [];
-  return basket;
 }//end empty
 console.log('The basket has: ', basket);
 console.log(addItem('chicken'));
 console.log('Checking new basket: ', basket);
-console.log(empty()); //this logs true to the console, so we can know that the function ran successfully.
+console.log(empty());
 console.log(basket); //array should now be empty!
 
 
@@ -84,9 +90,13 @@ console.log('Ooo BIG Stretch');
 //
 // 1. Add a global `const` named `maxItems` and set it to 5.
 
+//done
+
 // 2. Create a function called isFull(). It should:
 //   - return `false` if the basket contains *less* than max number of items
 //   - return `true` otherwise (equal or more than maxItems)
+
+//done
 
 // 3. Update the required `addItem` function to:
 //   - Use the `isFull` function to prevent more than `maxItems` from being added to the basket.
